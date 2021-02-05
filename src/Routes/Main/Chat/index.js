3fe4ce19be51ca10732777f5c588f8ff87b87a93
HYUNAHSHIM <hyunso8245@gmail.com>
2021-02-05 22:46:56 +0900

    출근 퇴근 구현

--- src/Routes/Main/Chat/index.js
+++ src/Routes/Main/Chat/index.js
@@ -1,4 +1,4 @@
-import React, {useState} from "react";
+import React, {useEffect, useState} from "react";
 import ChatPresenter from "./ChatPresenter";
 import axios from "axios";
 import { useSelector } from "react-redux";
@@ -7,34 +7,79 @@ export default ({ type, year, month, day, workers }) => {
     const [events, setEvents] = useState([]);
     const token = useSelector(state => state.user.userinfo.token);
     const userType = useSelector(state => state.user.userinfo.user_type);
+    const userId = useSelector(state => state.user.userinfo.user_id);
+    const today = new Date().toString().split(" ");
     const payload = {
         token: token,
         year: year,
         month: month,
         day: day
     }
-    const workplace_id = "workplace1";
+    const workplace_id = "1";
 
     // 특정 일에 출퇴근 여부 받아오기
-    /*axios.post('https://alba-api.herokuapp.com/workplace/' + workplace_id + '/attendance', payload ).then(response =>{
-        if(response.data.result == "Success"){
-            console.log("hi", response)
-            response.data.data.map(({date, employer_id, start_time, end_time, id, is_checked, name}) => {
-                setEvents(current => {
-                    const tmp = current;
-                    tmp.push({
-                        title: employer_id,
-                        allDay: false,
-                        start: new Date(date.split("-")[0], date.split("-")[1], date.split("-")[2], start_time.split(":")[0], start_time.split(":")[1]),
-                        end: new Date(date.split("-")[0], date.split("-")[1], date.split("-")[2], end_time.split(":")[0], end_time.split(":")[1])
+    useEffect(() => {
+        axios.post('https://alba-api.herokuapp.com/calendar/' + workplace_id, payload ).then(response =>{
+            if(response.data.result == "Success"){
+                setEvents(curr => {
+                    const tmp = [];
+                    response.data.data.map(({employee_id, start_time, end_time, is_checked}) => {
+                        tmp.push({
+                            name: employee_id,
+                            time: start_time.split(":")[0] + ":" + start_time.split(":")[1] + " ~ " + end_time.split(":")[0] + ":" + end_time.split(":")[1],
+                            isChecked: is_checked
+                        });
                     });
                     return tmp;
                 });
-            });
-        }else{
-            console.log(response)
-            alert("다시 시도해 주세요.");
+            }else{
+                console.log(response)
+                alert("다시 시도해 주세요.");
         }
-    });*/
-    return <ChatPresenter type={type} year={year} month={month} day={day} workers={workers} events={events} userType={userType}/>
+    });
+    }, [month, day]);
+
+    // 출퇴근 버튼 누르면 출퇴근 처리
+    const onClickButton = e => {
+        const id = e.target.id;
+        if (id === "출근") {
+            axios.patch('https://alba-api.herokuapp.com/workplace/' + workplace_id +'/attendance', payload ).then(response =>{
+            if(response.data.result == "Success"){
+                setEvents(curr => {
+                    const tmp = curr.map(element => {
+                        if(element.name === userId) {
+                            return {
+                                name: element.name,
+                                time: element.time,
+                                isChecked: 1
+                            }
+                        } else return element;
+                    });
+                    return tmp;
+                });
+            }else{
+                console.log(response)
+                alert("다시 시도해 주세요.");
+        }})} else if(id === "퇴근"){
+            axios.patch('https://alba-api.herokuapp.com/workplace/' + workplace_id +'/leave', payload ).then(response =>{
+                if(response.data.result == "Success"){
+                    setEvents(curr => {
+                        const tmp = curr.map(element => {
+                            if(element.name === userId) {
+                                return {
+                                    name: element.name,
+                                    time: element.time,
+                                    isChecked: 2
+                                }
+                            } else return element;
+                        });
+                        return tmp;
+                    });
+                }else{
+                    console.log(response)
+                    alert("다시 시도해 주세요.");
+            }
+        })}
+    }
+    return <ChatPresenter type={type} year={year} month={month} day={day} workers={workers} events={events} userType={userType} onClickButton={onClickButton}/>
 }
